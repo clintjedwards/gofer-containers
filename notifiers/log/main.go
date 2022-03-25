6906891c6b0fbdefebd6882d722fc306da20c67c
Clint J Edwards <clint.j.edwards@gmail.com>
2022-03-25 19:31:57 -0400

    removing run report from notifer

--- notifiers/log/main.go
+++ notifiers/log/main.go
@@ -2,37 +2,16 @@ package main
 
 import (
 	"log"
-	"os"
-
-	pb "github.com/clintjedwards/gofer/proto"
-	"google.golang.org/protobuf/proto"
 )
 
-// environment variables will be separated based on where they came from
-// GOFER_NOTIFIER_USER_CONFIG_<NAME>
-// GOFER_NOTIFIER_MAIN_CONFIG_<NAME>
-// Notifiers will also get two special environment variables unique to them
-// GOFER_NOTIFIER_RUN_REPORT=
-// GOFER_API_TOKEN=
-// The notifier run report is a byte string that can be turned into a proto.RunReport
-
-// getCurrentRun converts a json string of the current run into a run object.
-func getRunReport(report string) (*pb.RunReport, error) {
-	currentRun := &pb.RunReport{}
-	err := proto.Unmarshal([]byte(report), currentRun)
-	if err != nil {
-		return nil, err
-	}
-
-	return currentRun, nil
-}
+// Generally notifiers have several env vars that are passed in
+// 1) The env vars of the main config, which is set in the Gofer config file.
+// 	ex. "GOFER_NOTIFIER_MAIN_CONFIG_<VALUE>"
+// 2) The env vars of the user supplied config, which is set by users in the pipeline config file.
+// 	ex. "GOFER_NOTIFIER_USER_CONFIG_<VALUE>"
+// 3) The "GOFER_API_TOKEN" which is passed in so that the notifier can request more information about a particular
+// run.
 
 func main() {
-	reportString := os.Getenv("GOFER_NOTIFIER_RUN_REPORT")
-	report, err := getRunReport(reportString)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("notification successful for run %d\n", report.Id)
+	log.Printf("notification successful for run")
 }
